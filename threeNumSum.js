177ad6f75eabcbf8ca570e4dd6fc5d4a2a4cb3e4
Kerim <kutukmu@gmail.com>
2020-11-29 07:03:10 -0600

    threeSum Prompt

---
+++ threeNumSum.js
@@ -0,0 +1,8 @@
+// The function should find the all triplest in the array that sum up tp the target sum
+//and return two dimensional array of all triplets. The numbers in the each triblet should ne in ascending order
+//and triblets also should be ordered in ascending order
+// {"array": [12, 3, 1, 2, -6, 5, -8, 6], "targetSum": 0}
+// answer =  [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+
+
+
