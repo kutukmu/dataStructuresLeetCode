cf173bb163e8b6fda823e52451215214e07bb3d8
Kerim <kutukmu@gmail.com>
2020-11-30 16:47:17 -0600

    longest peak explanation

--- LongestPeak.js
+++ LongestPeak.js
@@ -3,3 +3,11 @@
 // longest peak = [0, 10, 6, 5, -1, -3, 2]
 // left side of an array strictly increasing until they reach a tip whcih is 10 
 //right side of an array strictly decreasin until 2
+
+
+// Explanation
+// we will iterate over an array and whenever we find a peak wil will start to open a window to left side and right side
+// left pointer will go till pointer gets a bigger value then previos index
+// right pointer will go till pointer gets a bigger value then previos index
+
+
