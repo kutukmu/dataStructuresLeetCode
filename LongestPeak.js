29173eaf78c6f7beca7615fa9312af432bff6ea0
Kerim <kutukmu@gmail.com>
2020-11-30 16:43:43 -0600

    longest peak definition

---
+++ LongestPeak.js
@@ -0,0 +1,5 @@
+// Write a fucntion that takes in a array and returns the longest peak in the array
+// "array": [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+// longest peak = [0, 10, 6, 5, -1, -3, 2]
+// left side of an array strictly increasing until they reach a tip whcih is 10 
+//right side of an array strictly decreasin until 2
