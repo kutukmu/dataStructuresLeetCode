ea92fb3a53e2833325b0612dec300c18ae852653
Kerim <kutukmu@gmail.com>
2020-11-30 10:47:32 -0600

    monoticarray explaniton

---
+++ MonotonicArray.js
@@ -0,0 +1,11 @@
+// Write a fucntion that take in an array of integers and return whether the array is monotonic
+
+
+
+//Solution Explanation
+//We will have a two varile that will keep whether array is increasing or decresing
+// and because montonic array can nto be increasing and decreasing both together 
+// if they are true at eh same time we will return false, otherwise return true
+
+
+
