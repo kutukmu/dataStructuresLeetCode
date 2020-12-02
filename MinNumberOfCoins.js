e2f1c5fd02f9d1f04197e298d3e3e4bdb522165d
Kerim <kutukmu@gmail.com>
2020-12-02 08:15:52 -0600

    minNumber Solution

--- MinNumberOfCoins.js
+++ MinNumberOfCoins.js
@@ -6,3 +6,37 @@ return -1.
 
 You may assume that you have an infinite number of each kind of coin.*/
 
+var coinChange = function(coins, amount) {
+    
+   
+    let arr = Array.from({length: amount + 1}).fill(Infinity);
+    
+    arr[0] = 0;
+    
+    
+    for(let i = 0; i < coins.length; i++){
+        
+        let denom  = coins[i];;
+        
+        for(let j = 0; j < arr.length; j++){
+            
+            
+            if(j >= denom){
+                
+              arr[j] = Math.min(arr[j], 1 + arr[j- denom])  
+                
+                
+            }
+            
+            
+            
+            
+            
+        }
+    }
+    
+    
+    
+    return arr[amount] === Infinity ? -1: arr[amount]
+
+};
