076c7457547ab64b8f6749336d3ab8cfd29345f7
Kerim <kutukmu@gmail.com>
2020-12-06 09:16:43 -0600

    SearchMatrix Solution

--- searchIn2dMatrix.js
+++ searchIn2dMatrix.js
@@ -5,3 +5,36 @@ Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
 */
 
+
+//Solution
+
+var searchMatrix = function(matrix, target) {
+    
+    
+    if(!matrix.length){
+        return false
+    }
+    
+    
+    let i = 0;
+    let j = matrix[0].length - 1
+    
+    while(i < matrix.length && j >= 0){
+        
+        
+        if(matrix[i][j] > target){
+            j--
+        }else if(matrix[i][j] < target){
+            i++
+        }else{
+            return true
+        }
+        
+        
+        
+    }
+    
+    
+    return false
+ 
+};
