9b3d934ba22c3c9d89417762cfefbce0b8096be4
Kerim <kutukmu@gmail.com>
2020-12-08 12:58:37 -0600

    subarraysort expalantion

---
+++ ShortestUnsortedContiSub.js
@@ -0,0 +1,16 @@
+/*
+Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
+Return the shortest such subarray and output its length.
+*/
+
+// Explanation
+//if we find the min unsrtoed element and max unsorted element
+// the subarray we need to srot is actually, their correct location
+// [2,6,4,8,10,9,15]
+// 4 is min 
+// 10 is max
+// location of 4 is index = 1;
+// location of 10 is index  = 5
+// asnwer  5 - 1 + 1;
+
+
